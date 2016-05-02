4f4954abb275680f2cf094e1a6383b5a77d7ccd9
Mikhail Novozhilov <mixnov@bk.ru>
2016-05-02 15:42:51 +1200

    Copy the the DB file to public folder and start with ShopActivity

--- app/src/main/java/nz/co/novozhilov/mikhail/haveaniceprice/ShopActivity.java
+++ app/src/main/java/nz/co/novozhilov/mikhail/haveaniceprice/ShopActivity.java
@@ -1,33 +1,51 @@
 package nz.co.novozhilov.mikhail.haveaniceprice;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import nz.co.novozhilov.mikhail.haveaniceprice.fargments.ShopsListFragment;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Mikhail on 29.04.2016.
  */
 public class ShopActivity extends AppCompatActivity {
+
+    private static final String TAG = ShopActivity.class.getName();
+
     public static final String EXTRA_SHOP = "SHOP";
     private Shop shop;
     private WebView webView;
+    private TextView link, title;
+    private Button add;
+
+    public Elements elements;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        link = (TextView) findViewById(R.id.txtLink);
+        add = (Button) findViewById(R.id.btnAdd);
+        title = (TextView) findViewById(R.id.txtTitle);
+        add.setEnabled(false);
         Bundle extras = this.getIntent().getExtras();
         shop = extras.getParcelable(EXTRA_SHOP);
         setTitle(shop.getTitle());
--- app/src/main/java/nz/co/novozhilov/mikhail/haveaniceprice/db/DBHelper.java
+++ app/src/main/java/nz/co/novozhilov/mikhail/haveaniceprice/db/DBHelper.java
@@ -3,12 +3,22 @@ package nz.co.novozhilov.mikhail.haveaniceprice.db;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.File;
+
+import android.os.Environment;
+
 import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 import android.annotation.SuppressLint;
 import android.util.Log;
 
@@ -19,7 +29,7 @@ import android.util.Log;
  */
 public final class DBHelper extends SQLiteOpenHelper {
 
-    private static final String TAG = DBHelper.class.getName();
+    private static final String LOG_TAG = DBHelper.class.getName();
 
     private static final int DB_VERSION = 1; // DB version (used on upgrade)
     private static String DB_PATH = "/data/data/";
@@ -30,39 +40,45 @@ public final class DBHelper extends SQLiteOpenHelper {
     private Context mContext;
 
     // Tables
-    public static final String TABLE_PRODUCTS       = "products";
-    public static final String TABLE_SHOPS          = "shops";
-    public static final String TABLE_STAT           = "stat";
-    public static final String TABLE_USER_PROFILE   = "user_profile";
+    public static final String TABLE_PRODUCTS = "products";
+    public static final String TABLE_SHOPS = "shops";
+    public static final String TABLE_STAT = "stat";
+    public static final String TABLE_USER_PROFILE = "user_profile";
 
     // Common columns names
-    public static final String COLUMN_ID            = "_id";
+    public static final String COLUMN_ID = "_id";
 
     //  Products table columns
-    public static final String COLUMN_P_SHOP_ID     = "shop_id";
-    public static final String COLUMN_P_IMG_URL     = "img_url";
-    public static final String COLUMN_P_TITLE       = "title";
-    public static final String COLUMN_P_URL         = "url";
+    public static final String COLUMN_P_SHOP_ID = "shop_id";
+    public static final String COLUMN_P_IMG_URL = "img_url";
+    public static final String COLUMN_P_TITLE = "title";
+    public static final String COLUMN_P_URL = "url";
 
     //  Shops table columns
-    public static final String COLUMN_SH_TITLE      = "title";
-    public static final String COLUMN_SH_URL        = "url";
-    public static final String COLUMN_SH_DIV_TITLE  = "div_title";
-    public static final String COLUMN_SH_TITLE_IN   = "title_in";
-    public static final String COLUMN_SH_STD_PRICE  = "std_price";
+    public static final String COLUMN_SH_TITLE = "title";
+    public static final String COLUMN_SH_URL = "url";
+    public static final String COLUMN_SH_DIV_TITLE = "div_title";
+    public static final String COLUMN_SH_TITLE_IN = "title_in";
+    public static final String COLUMN_SH_STD_PRICE = "std_price";
     public static final String COLUMN_SH_DISC_PRICE = "disc_price";
-    public static final String COLUMN_SH_OLD_PRICE  = "old_price";
+    public static final String COLUMN_SH_OLD_PRICE = "old_price";
 
     // Stat table columns
     public static final String COLUMN_ST_PRODUCT_ID = "product_id";
-    public static final String COLUMN_ST_DATE       = "date";
-    public static final String COLUMN_ST_STD_PRICE  = "std_price";
+    public static final String COLUMN_ST_DATE = "date";
+    public static final String COLUMN_ST_STD_PRICE = "std_price";
     public static final String COLUMN_ST_DISC_PRICE = "disc_price";
-    public static final String COLUMN_ST_OLD_PRICE  = "old_price";
+    public static final String COLUMN_ST_OLD_PRICE = "old_price";
 
     // User profile table columns
     public static final String COLUMN_UP_NAME = "name";
     public static final String COLUMN_UP_PASS = "pass";
+
+    final String FILENAME = "file";
+
+    final String DIR_SD = "my_files/database/";
+
+    private static String fullDBFileName;
 
 
     @SuppressLint("SdCardPath")
@@ -70,94 +86,8 @@ public final class DBHelper extends SQLiteOpenHelper {
         super(context, DB_NAME, null, DB_VERSION);
 
         this.mContext = context;
-        DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        fullDBFileName = "/data/data/" + context.getPackageName() + "/databases/" + DB_NAME;
         openDataBase();
-    }
-
-    /**
-     * Run copy database from assets if not exists
-     */
-    public void createDB(){
-        InputStream myInput = null;
-        OutputStream myOutput = null;
-        try {
-//            File file = new File(DB_PATH + DB_NAME);
-//            if (!file.exists()) {
-                this.getReadableDatabase();
-                //получаем локальную бд как поток
-                myInput = mContext.getAssets().open(DB_NAME);
-                // Путь к новой бд
-                String outFileName = DB_PATH + DB_NAME;
-
-                // Открываем пустую бд
-                myOutput = new FileOutputStream(outFileName);
-
-                // побайтово копируем данные
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = myInput.read(buffer)) > 0) {
-                    myOutput.write(buffer, 0, length);
-                }
-
-                myOutput.flush();
-                myOutput.close();
-                myInput.close();
-//            }
-        }
-        catch(IOException ex){
-
-        }
-
-//        try {
-//            copyDataBase();
-//        } catch (IOException e) {
-//            Log.e(TAG, "Copy database failed!");
-//        }
-//        boolean dbExist = checkDataBase();
-//        if(dbExist){
-//            Log.v(TAG, "database does exist");
-//        }else{
-//            Log.v(TAG, "database does not exist");
-//            this.getReadableDatabase();
-//            try {
-//                copyDataBase();
-//            } catch (IOException e) {
-//                Log.e(TAG, "Copy database failed!");
-//            }
-//        }
-    }
-
-    /**
-     * Copy database from assets to the app data
-     * @throws IOException
-     */
-    private void copyDataBase() throws IOException{
-        // get source and target streams
-        InputStream myInput = mContext.getAssets().open(DB_NAME);
-
-        String outFileName = DB_PATH + DB_NAME;
-        OutputStream myOutput = new FileOutputStream(outFileName);
-
-        // copy file
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = myInput.read(buffer))>0){
-            myOutput.write(buffer, 0, length);
-        }
-        // close streams
-        myOutput.flush();
-        myOutput.close();
-        myInput.close();
-    }
-
-    /**
-     * Check if source db is created
-     *
-     * @return true if exists
-     */
-    private boolean checkDataBase(){
-        File dbFile = new File(DB_PATH + DB_NAME);
-        return dbFile.exists();
     }
 
     /**
@@ -166,16 +96,79 @@ public final class DBHelper extends SQLiteOpenHelper {
      * @return database opened for writing and reading
      */
     public boolean openDataBase() {
-        String mPath = DB_PATH + DB_NAME;
-        createDB();
+        //createDB();
         if (!checkDataBase()) {
             createDB();
         }
         if (mDataBase == null) {
-            mDataBase = SQLiteDatabase.openDatabase(mPath, null, SQLiteDatabase.OPEN_READWRITE);
+            try {
+                writeFileSD();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mDataBase = SQLiteDatabase.openDatabase(fullDBFileName, null, SQLiteDatabase.OPEN_READWRITE);
         }
 
         return mDataBase.isOpen();
+    }
+
+    /**
+     * Check if source db is created
+     *
+     * @return true if exists
+     */
+    private boolean checkDataBase() {
+        File dbFile = new File(fullDBFileName);
+        return dbFile.exists();
+    }
+
+    /**
+     * Run copy database from assets if not exists
+     */
+    public void createDB() {
+
+        try {
+            copyDataBase();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Copy database failed!");
+        }
+        boolean dbExist = checkDataBase();
+        if(dbExist){
+            Log.v(LOG_TAG, "database does exist");
+        }else{
+            Log.v(LOG_TAG, "database does not exist");
+            this.getReadableDatabase();
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Copy database failed!");
+            }
+        }
+    }
+
+    /**
+     * Copy database from assets to the app data
+     *
+     * @throws IOException
+     */
+    private void copyDataBase() throws IOException {
+        InputStream myInput = null;
+        OutputStream myOutput = null;
+        this.getReadableDatabase();
+        // get source and target streams
+        myInput = mContext.getAssets().open(DB_NAME);
+        myOutput = new FileOutputStream(fullDBFileName);
+
+        // copy file
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
     }
 
     /**
@@ -183,7 +176,7 @@ public final class DBHelper extends SQLiteOpenHelper {
      */
     @Override
     public synchronized void close() {
-        if(mDataBase != null)
+        if (mDataBase != null)
             mDataBase.close();
         super.close();
     }
@@ -194,5 +187,74 @@ public final class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    void writeFileSD()  throws IOException {
+        // проверяем доступность SD
+        if (!Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+        boolean res;
+        res = sdPath.mkdirs();
+        // добавляем свой каталог к пути
+        String sdFullDBFileName = sdPath.getAbsolutePath() + "/" + DB_NAME;
+        InputStream myInput = null;
+        OutputStream myOutput = null;
+//        this.getReadableDatabase();
+        // get source and target streams
+        myInput = new FileInputStream(fullDBFileName);
+        myOutput = new FileOutputStream(sdFullDBFileName);
+
+        // copy file
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    private String getSDcardPath()
+    {
+        String exts =  Environment.getExternalStorageDirectory().getPath();
+        String sdCardPath = null;
+        try
+        {
+            FileReader fr = new FileReader(new File("/proc/mounts"));
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine())!=null)
+            {
+                if(line.contains("secure") || line.contains("asec"))
+                    continue;
+                if(line.contains("fat"))
+                {
+                    String[] pars = line.split("\\s");
+                    if(pars.length<2)
+                        continue;
+                    if(pars[1].equals(exts))
+                        continue;
+                    sdCardPath =pars[1];
+                    break;
+                }
+            }
+            fr.close();
+            br.close();
+            return sdCardPath;
+        }
+        catch (Exception e)
+        {
+            //e.printStackTrace();
+            Log.v(LOG_TAG, e.toString());
+        }
+        return sdCardPath;
     }
 }
